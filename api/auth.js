afc9a55bcd2576206aabee161caa4210af2a184d
Jeff Shapiro <jeffreyhshapiro@gmail.com>
2018-01-09 09:06:58 -0500

    modularize auth

---
+++ api/auth.js
@@ -0,0 +1,57 @@
+const LocalStrategy = require('passport-local').Strategy;
+const models = require('../models');
+const bcrypt = require('bcrypt');
+
+module.exports = (passport) => {
+
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function (user, done) {
+        done(null, user);
+    });
+
+    passport.use('local-signin',
+        new LocalStrategy({
+            usernameField: 'emailAddress',
+            passwordField: 'password'
+        },
+            (username, password, done) => {
+
+                models.User.findOne({
+                    where: {
+                        emailAddress: username
+                    }
+                })
+                    .then((res) => {
+                        return bcrypt
+                            .compare(password, res.password)
+                            .then((doesPasswordMatch) => {
+                                if (doesPasswordMatch) {
+                                    return {
+                                        firstName: res.firstName,
+                                        lastName: res.lastName,
+                                        emailAddress: res.emailAddress,
+                                        _id: res.id
+                                    }
+                                } else {
+                                    return false
+                                }
+                            })
+                    })
+                    .then((success) => {
+                        if (success) {
+                            done(null, success)
+                        } else {
+                            return done(null, false, { message: "Your username or password is incorrect" })
+                        }
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            }
+        )
+    )
+
+}
