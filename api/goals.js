0051ff5a37d847b18e38d25d4f5e7c3a04254695
Jeff Shapiro <jeffreyhshapiro@gmail.com>
2018-02-16 09:02:14 -0500

    update session object with new goal info

--- api/goals.js
+++ api/goals.js
@@ -1,17 +1,26 @@
 const Goal = require('../models').Goal;
+const passport = require('passport');
 
 module.exports = (app) => {
 
     app.post('/api/goal/createGoal', (req, res) => {
-
         const { userId, goal } = req.body;
 
         Goal.create({
             UserId: userId,
             goal,
-            completed: false            
+            completed: false
         }).then((resp) => {
+            //update session object
+            const { goals } = req.session.passport.user;
+
+            req.session.passport.user.goals = [
+                ...goals,
+                resp
+            ]
+
             res.status(200).json(resp);
+
         })
     });
 
