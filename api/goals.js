331da18070b2c75426f71c8180aa0ff6b0b34088
Jeff Shapiro <jeffreyhshapiro@gmail.com>
2018-02-25 20:26:56 -0500

    include the Friends array whether sequelize wants me to do it or not

--- api/goals.js
+++ api/goals.js
@@ -1,28 +1,42 @@
 const Goal = require('../models').Goal;
 const passport = require('passport');
+const Friend = require('../models').Friend;
 
 module.exports = (app) => {
 
     app.post('/api/goal/createGoal', (req, res) => {
         const { userId, goal, deadline } = req.body;
 
-        Goal.create({
+        const create = {
             UserId: userId,
             goal,
             deadline: deadline,
             completed: false
-        }).then((resp) => {
-            //update session object
-            const { goals } = req.session.passport.user;
+        }
 
-            req.session.passport.user.goals = [
-                ...goals,
-                resp
-            ]
+        Goal.create(create)
+            .then((resp) => {
 
-            res.status(200).json(resp);
+            return [ Friend.findAll({
+                    where: {
+                        GoalId: resp.id
+                    }
+                }), resp]
 
-        })
+            }).spread((friends, newGoal) => {
+
+                //a less than ideal solution to put the friends array in the Goal response object but it works for now ¯\_(ツ)_/¯
+                newGoal.dataValues.Friends = friends;
+
+                //update session object
+                const { goals } = req.session.passport.user;
+
+                req.session.passport.user.goals = [
+                    ...goals,
+                    newGoal
+                ]
+                res.status(200).json(newGoal);
+            })
     });
 
 }
